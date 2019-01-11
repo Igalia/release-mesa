Grima04/mesa-turnip-kgsl

commit e0dbfc995370756355f28ac31495eab96a410384

bin/get-pick-list.sh: warn when commit lists invalid sha
We had cases where people would list old/invalid sha in the commit.
Add a trivial checker to catch those and throw a warning.

CC: Juan A. Suarez <jasuarez@igalia.com>
CC: Dylan Baker <dylan@pnwbakers.com>
CC: mesa-stable@lists.freedesktop.org
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Andres Gomez <agomez@igalia.com>

bin/get-pick-list.sh
@@ -38,6 +38,17 @@ is_sha_nomination()
 	if test $fixes_count -eq 0; then
 		return 1
 	fi
+
+	# Throw a warning for each invalid sha
+	while test $fixes_count -gt 0; do
+		# Treat only the current line
+		id=`echo "$fixes" | tail -n $fixes_count | head -n 1 | cut -d : -f 2`
+		fixes_count=$(($fixes_count-1))
+		if ! git show $id &>/dev/null; then
+			echo WARNING: Commit $1 lists invalid sha $id
+		fi
+	done
+
 	return 0
 }
 

Igalia/release-mesa

commit 69202273a1f4e7c512222a90680aeeec0ca41a11

bin/get-pick-list.sh: fix redirection in sh
"&>" is bash specific.

Fixes: e0dbfc99537 ("bin/get-pick-list.sh: warn when commit lists invalid sha")
Cc: Juan A. Suarez <jasuarez@igalia.com>
Cc: Eric Engestrom <eric.engestrom@intel.com>
Cc: Dylan Baker <dylan@pnwbakers.com>
Cc: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Andres Gomez <agomez@igalia.com>

bin/get-pick-list.sh
@@ -44,7 +44,7 @@ is_sha_nomination()
 		# Treat only the current line
 		id=`echo "$fixes" | tail -n $fixes_count | head -n 1 | cut -d : -f 2`
 		fixes_count=$(($fixes_count-1))
-		if ! git show $id &>/dev/null; then
+		if ! git show $id >/dev/null 2>&1; then
 			echo WARNING: Commit $1 lists invalid sha $id
 		fi
 	done
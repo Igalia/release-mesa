anholt/mesa

commit 559c32d2412b2ea602bb59aa61da75403d01a872

bin/get-pick-list.sh: simplify git oneline printing
Currently we force disable the pager via "|cat" where --no-pager
exists. Additionally we could use git show instead of git log -n1.

Use those for a slightly more understandable code.

Cc: mesa-stable@lists.freedesktop.org
Reviewed-by: Juan A. Suarez <jasuarez@igalia.com>
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

bin/get-pick-list.sh
@@ -32,7 +32,7 @@ do
 		continue
 	fi
 
-	git log -n1 --pretty=oneline $sha | cat
+	git --no-pager show --summary --oneline $sha
 done
 
 rm -f already_picked

Igalia/release-mesa

commit 94e86e1ad4bf3d6d0066c867189712ff5561e2e2

bin/get-pick-list.sh: fix the oneline printing
"--summary" will also print extended header information such as
creations, renames and mode changes.

Let's just use "-s", which suppresses the diff output.

Fixes: 559c32d2412 ("bin/get-pick-list.sh: simplify git oneline printing")
Cc: Juan A. Suarez <jasuarez@igalia.com>
Cc: Eric Engestrom <eric.engestrom@intel.com>
Cc: Dylan Baker <dylan@pnwbakers.com>
Cc: Emil Velikov <emil.velikov@collabora.com>
Signed-off-by: Andres Gomez <agomez@igalia.com>

bin/get-pick-list.sh
@@ -143,7 +143,7 @@ do
 	esac
 
 	printf "[ %8s ] " "$tag"
-	git --no-pager show --summary --oneline $sha
+	git --no-pager show -s --oneline $sha
 done
 
 rm -f already_picked